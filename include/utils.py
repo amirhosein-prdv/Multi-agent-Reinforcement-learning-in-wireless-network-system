9a507d902fd87615d89ec82dd62addcf6f669b98
amirhosein <ah.pourdavoud79@gmail.com>
2024-02-10 19:10:44 +0330

    Edit utils

--- include/utils.py
+++ include/utils.py
@@ -1,66 +1,14 @@
 import numpy as np
-from collections import OrderedDict
+import copy
+import matplotlib.pyplot as plt
 
-import torch
-import torch.nn as nn
+class DotDic(dict):
+	__getattr__ = dict.get
+	__setattr__ = dict.__setitem__
+	__delattr__ = dict.__delitem__
 
-
-# Expects tuples of (state, next_state, action, reward, done)
-class ReplayBuffer(object):
-	def __init__(self, max_size=1e4):
-		self.storage = []
-		self.max_size = max_size
-		self.ptr = 0
-
-	def add(self, data):
-		if len(self.storage) == self.max_size:
-			self.storage[int(self.ptr)] = data
-			self.ptr = (self.ptr + 1) % self.max_size
-		else:
-			self.storage.append(data)
-
-	def sample(self, batch_size):
-		ind = np.random.randint(0, len(self.storage), size=batch_size)
-		x, y, u, r, d = [], [], [], [], []
-
-		for i in ind: 
-			X, Y, U, R, D = self.storage[i]
-			x.append(np.array(X, copy=False))
-			y.append(np.array(Y, copy=False))
-			u.append(np.array(U, copy=False))
-			r.append(np.array(R, copy=False))
-			d.append(np.array(D, copy=False))
-
-		return np.array(x), np.array(y), np.array(u), np.array(r).reshape(-1, 1), np.array(d).reshape(-1, 1)
-
-class Hot_Plug(object):
-    def __init__(self, model):
-        self.model = model
-        self.params = OrderedDict(self.model.named_parameters())
-    def update(self, lr=0.1):
-        for param_name in self.params.keys():
-            path = param_name.split('.')
-            cursor = self.model
-            for module_name in path[:-1]:
-                cursor = cursor._modules[module_name]
-            if lr > 0:
-                cursor._parameters[path[-1]] = self.params[param_name] - lr*self.params[param_name].grad
-            else:
-                cursor._parameters[path[-1]] = self.params[param_name]
-    def restore(self):
-        self.update(lr=0)
-
-class Critic_Network(nn.Module):
-    def __init__(self, hidden_dim):
-        super(Critic_Network, self).__init__()
-        self.fc1 = nn.Linear(hidden_dim,100)
-        self.fc2 = nn.Linear(100,100)
-        self.fc3 = nn.Linear(100,1)
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = nn.functional.softplus(self.fc3(x))
-        return torch.mean(x)
+	def __deepcopy__(self, memo=None):
+		return DotDic(copy.deepcopy(dict(self), memo=memo))
 
 
 def sliding_mean(data_array, window=5):
@@ -76,3 +24,13 @@ def sliding_mean(data_array, window=5):
         new_list.append(avg)
 
     return np.array(new_list)
+
+def plot_learning_curve(scores):
+    x = [i+1 for i in range(len(scores))]
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_avg)
+    plt.title('Rewards versus episodes')
+    plt.xlabel('episode'); plt.ylabel('total rewards')
+    #plt.savefig(figure_file)
