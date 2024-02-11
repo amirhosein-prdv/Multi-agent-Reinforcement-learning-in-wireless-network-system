4e74b6b2da5510bc2ce92dc954fc9a7cbfb2572e
amirhosein <ah.pourdavoud79@gmail.com>
2024-02-12 01:09:03 +0330

    [ADD] SAC algorithm implementation added

---
+++ include/SAC.py
@@ -0,0 +1,306 @@
+import os
+import numpy as np
+
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions.normal import Normal
+
+class ReplayBuffer():
+    def __init__(self, max_size, state_dim, action_dim):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.state_memory = np.zeros((self.mem_size, state_dim))
+        self.new_state_memory = np.zeros((self.mem_size, state_dim))
+        self.action_memory = np.zeros((self.mem_size, action_dim))
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
+
+    def store_transition(self, state, state_, action, reward, done):
+        index = self.mem_cntr % self.mem_size
+
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+
+        self.mem_cntr += 1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+
+        batch = np.random.choice(max_mem, batch_size)
+
+        states = self.state_memory[batch]
+        states_ = self.new_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
+
+        return states, states_, actions, rewards, dones
+
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action, hidden_dims, lr, name='actor', chkpt_dir='SAC Training'):
+        super(Actor, self).__init__()
+        self.state_dim = state_dim
+        self.fc1_dims = hidden_dims[0]
+        self.fc2_dims = hidden_dims[1]
+        self.action_dim = action_dim
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+        self.max_action = max_action
+        self.reparam_noise = 1e-6
+
+        self.fc1 = nn.Linear(self.state_dim, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.mu = nn.Linear(self.fc2_dims, self.action_dim)
+        self.sigma = nn.Linear(self.fc2_dims, self.action_dim)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def forward(self, state):
+        prob = self.fc1(state)
+        prob = F.relu(prob)
+        prob = self.fc2(prob)
+        prob = F.relu(prob)
+
+        mu = self.mu(prob)
+        sigma = self.sigma(prob)
+
+        sigma = T.clamp(sigma, min=self.reparam_noise, max=1)
+
+        return mu, sigma
+
+    def sample_normal(self, state, reparameterize=True):
+        mu, sigma = self.forward(state)
+        probabilities = Normal(mu, sigma)
+
+        if reparameterize:
+            actions = probabilities.rsample()
+        else:
+            actions = probabilities.sample()
+
+        action = T.tanh(actions) * T.tensor(self.max_action).to(self.device)
+        log_probs = probabilities.log_prob(actions)
+        log_probs -= T.log(1-action.pow(2)+self.reparam_noise)
+        log_probs = log_probs.sum(1, keepdim=True)
+
+        return action, log_probs
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dims, lr, name='critic', chkpt_dir='SAC Training'):
+        super(Critic, self).__init__()
+        self.state_dim = state_dim
+        self.fc1_dims = hidden_dims[0]
+        self.fc2_dims = hidden_dims[1]
+        self.action_dim = action_dim
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+
+        self.fc1 = nn.Linear(self.state_dim+action_dim, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.q = nn.Linear(self.fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def forward(self, state, action):
+        action_value = self.fc1(T.cat([state, action], dim=1))
+        action_value = F.relu(action_value)
+        action_value = self.fc2(action_value)
+        action_value = F.relu(action_value)
+
+        q = self.q(action_value)
+
+        return q
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+class ValueNetwork(nn.Module):
+    def __init__(self, state_dim, hidden_dims, lr, name='value', chkpt_dir='SAC Training'):
+        super(ValueNetwork, self).__init__()
+        self.state_dim = state_dim
+        self.fc1_dims = hidden_dims[0]
+        self.fc2_dims = hidden_dims[1]
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+
+        self.fc1 = nn.Linear(self.state_dim, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.v = nn.Linear(self.fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def forward(self, state):
+        state_value = self.fc1(state)
+        state_value = F.relu(state_value)
+        state_value = self.fc2(state_value)
+        state_value = F.relu(state_value)
+
+        v = self.v(state_value)
+
+        return v
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+
+
+class SAC():
+    def __init__(self, state_dim, action_dim, max_action, args):
+        self.args = args
+
+        self.memory = ReplayBuffer(self.args.buffer_size, state_dim, action_dim)
+
+        self.actor = Actor(state_dim, action_dim, max_action, self.args.actor_hidden, self.args.actor_lr,\
+                            name='actor', chkpt_dir=self.args.model_path)
+
+        self.critic_1 = Critic(state_dim, action_dim, self.args.critic_hidden, self.args.critic_lr,\
+                                name='critic_1', chkpt_dir=self.args.model_path)
+        self.critic_2 = Critic(state_dim, action_dim, self.args.critic_hidden, self.args.critic_lr,\
+                                name='critic_2', chkpt_dir=self.args.model_path)
+
+        self.value = ValueNetwork(state_dim, self.args.value_hidden, self.args.value_lr, \
+                                   name='value', chkpt_dir=self.args.model_path)
+        self.target_value = ValueNetwork(state_dim, self.args.value_hidden, self.args.value_lr, \
+                                          name='target_value', chkpt_dir=self.args.model_path)
+
+        self.scale = self.args.reward_scale
+        self.update_network_parameters(tau=1)
+
+    def select_action(self, observation):
+        state = T.Tensor([observation]).to(self.actor.device)
+        actions, _ = self.actor.sample_normal(state, reparameterize=False)
+
+        return actions.cpu().detach().numpy()[0]
+
+    def remember(self, state, new_state, action, reward, done):
+        self.memory.store_transition(state, new_state, action, reward, done)
+
+    def update_network_parameters(self, tau=None):
+        if tau is None:
+            tau = self.args.tau
+
+        target_value_params = self.target_value.named_parameters()
+        value_params = self.value.named_parameters()
+
+        target_value_state_dict = dict(target_value_params)
+        value_state_dict = dict(value_params)
+
+        for name in value_state_dict:
+            value_state_dict[name] = tau*value_state_dict[name].clone() + \
+                    (1-tau)*target_value_state_dict[name].clone()
+
+        self.target_value.load_state_dict(value_state_dict)
+
+    
+
+    def train(self):
+        if self.memory.mem_cntr < self.args.batch_size:
+            return
+
+        state, new_state, action, reward, done = self.memory.sample_buffer(self.args.batch_size)
+
+        reward = T.tensor(reward, dtype=T.float).to(self.actor.device)
+        done = T.tensor(done).to(self.actor.device)
+        state_ = T.tensor(new_state, dtype=T.float).to(self.actor.device)
+        state = T.tensor(state, dtype=T.float).to(self.actor.device)
+        action = T.tensor(action, dtype=T.float).to(self.actor.device)
+
+        value = self.value(state).view(-1)
+        value_ = self.target_value(state_).view(-1)
+        value_[done] = 0.0
+
+        actions, log_probs = self.actor.sample_normal(state, reparameterize=False)
+        log_probs = log_probs.view(-1)
+        q1_new_policy = self.critic_1.forward(state, actions)
+        q2_new_policy = self.critic_2.forward(state, actions)
+        critic_value = T.min(q1_new_policy, q2_new_policy)
+        critic_value = critic_value.view(-1)
+
+        self.value.optimizer.zero_grad()
+        value_target = critic_value - log_probs
+        value_loss = 0.5 * F.mse_loss(value, value_target)
+        value_loss.backward(retain_graph=True)
+        self.value.optimizer.step()
+
+        actions, log_probs = self.actor.sample_normal(state, reparameterize=True)
+        log_probs = log_probs.view(-1)
+        q1_new_policy = self.critic_1.forward(state, actions)
+        q2_new_policy = self.critic_2.forward(state, actions)
+        critic_value = T.min(q1_new_policy, q2_new_policy)
+        critic_value = critic_value.view(-1)
+        
+        # calculate actor loss and optimize
+        actor_loss = log_probs - critic_value
+        actor_loss = T.mean(actor_loss)
+        self.actor.optimizer.zero_grad()
+        actor_loss.backward(retain_graph=True)
+        self.actor.optimizer.step()
+
+        self.critic_1.optimizer.zero_grad()
+        self.critic_2.optimizer.zero_grad()
+        q_hat = self.scale*reward + self.args.gamma * value_
+        q1_old_policy = self.critic_1.forward(state, action).view(-1)
+        q2_old_policy = self.critic_2.forward(state, action).view(-1)
+        critic_1_loss = 0.5 * F.mse_loss(q1_old_policy, q_hat)
+        critic_2_loss = 0.5 * F.mse_loss(q2_old_policy, q_hat)
+
+        critic_loss = critic_1_loss + critic_2_loss
+        critic_loss.backward()
+        self.critic_1.optimizer.step()
+        self.critic_2.optimizer.step()
+
+        self.update_network_parameters()
+
+
+    def save(self):
+        print('.... saving models ....')
+        self.actor.save_checkpoint()
+        self.value.save_checkpoint()
+        self.target_value.save_checkpoint()
+        self.critic_1.save_checkpoint()
+        self.critic_2.save_checkpoint()
+        print("====================================")
+        print(" Model has been saved...")
+        print("====================================")
+
+    def load(self):
+        self.actor.load_checkpoint()
+        self.value.load_checkpoint()
+        self.target_value.load_checkpoint()
+        self.critic_1.load_checkpoint()
+        self.critic_2.load_checkpoint()
+        print("====================================")
+        print(" model has been loaded...")
+        print("====================================")
+
+
